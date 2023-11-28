5488ae4a4d2ae1fa714176d1c0fb1785aaecad21
popovon0325@gmail.com <popovon0325@gmail.com>
2023-11-28 16:05:55 +0900

    スキルの所に画像を入れる作業がこれから起きるので、画像の為にコミットを済ませておく。

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
     var imageList = document.getElementById("imageList");
     var imageContainers = imageList.getElementsByClassName("image-container");
-    var images = imageList.getElementsByClassName("image");
 
-
-    var imageList = document.getElementById("imageList");
-    var imageContainers = imageList.getElementsByClassName("image-container");
-    var images = imageList.getElementsByClassName("image");
-
-    for (var i = 0; i < images.length; i++) {
+    for (var i = 0; i < imageList.children.length; i++) {
+        var images = imageContainers[i].getElementsByClassName("image");  // この行を修正
         var imageText = imageContainers[i].getElementsByClassName("image-text")[0];
+        var gitLink = imageContainers[i].getElementsByClassName("git-links")[0];
         
+        gitLink.style.marginBottom = "0%";
         if (i % 2 === 0) {
-            images[i].style.marginRight = "auto";
-            images[i].style.marginLeft = "7.5%"; // 左寄せの画像は左マージンを自動に設定
-            console.log("Image " + (i + 1) + ":Left");
-            imageContainers[i].style.background = 'linear-gradient(to left,#FFF0B8,#FFE08B)';
-        } 
-        else {
-            images[i].style.marginLeft = "auto";
-            images[i].style.marginRight = "7.5%"; // 右寄せの画像は右マージンを自動に設定
-            console.log("Image " + (i + 1) + ": Right");
-            imageContainers[i].style.background = 'linear-gradient(to right,#CEF5FF,#B7BBFF)';
+            images[0].style.marginRight = "auto";  // 修正
+            images[0].style.paddingLeft = "10%";
+            imageContainers[i].style.background = 'linear-gradient(to left,#FFF0B8,#FFD782,#FFD782)';
+            imageText.style.left = "30%";
+            gitLink.style.right = "20%";
+        } else {
+            images[0].style.marginLeft = "auto";  // 修正
+            images[0].style.paddingRight = "10%";
+            imageContainers[i].style.background = 'linear-gradient(to right,#CEF5FF,#E7C7FF)';
+            imageText.style.right = "30%";
+            gitLink.style.left = "20%";
         }
     }
 });
