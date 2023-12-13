8c4aa368c35761bd4d7beb698c30a553632c55e4
popovon0325@gmail.com <popovon0325@gmail.com>
2023-12-13 21:19:54 +0900

    ColorBulletpngを導入しました。

--- main.js
+++ main.js
@@ -7,19 +7,33 @@ document.addEventListener("DOMContentLoaded", function() {
         var imageText = imageContainers[i].getElementsByClassName("image-text")[0];
         var gitLink = imageContainers[i].getElementsByClassName("git-links")[0];
         
-        gitLink.style.marginBottom = "0%";
+        gitLink.style.marginBottom = "-4.275%";
         if (i % 2 === 0) {
             images[0].style.marginRight = "auto";  // 修正
-            images[0].style.paddingLeft = "10%";
+            images[0].style.marginLeft = "10%";
             imageContainers[i].style.background = 'linear-gradient(to left,#FFF0B8,#FFF0B8,#FFD782)';
             imageText.style.left = "30%";
-            gitLink.style.right = "17%";
+            gitLink.style.right = "22%";
         } else {
             images[0].style.marginLeft = "auto";  // 修正
-            images[0].style.paddingRight = "10%";
+            images[0].style.marginRight = "10%";
             imageContainers[i].style.background = 'linear-gradient(to right,#CEF5FF,#E7C7FF)';
             imageText.style.right = "30%";
-            gitLink.style.left = "17%";
+            gitLink.style.left = "27%";
+        }
+    }
+});
+let targets = document.querySelectorAll('.js-scroll-curtain');
+let isSet = false;
+window.addEventListener('scroll', function () {
+    var scroll = window.scrollY;
+    var windowHeight = window.innerHeight;
+    for (let target of targets) {
+        var targetPos = target.getBoundingClientRect().top + scroll;
+
+        if (scroll > targetPos - windowHeight && !isSet) {
+            isSet = true;
+            console.log("aaa");
         }
     }
 });
