ebeee0fa5203a3eecbd7ae4417c9cf584f6be317
popovon0325@gmail.com <popovon0325@gmail.com>
2024-05-22 00:01:02 +0900

    ポートフォリオの進捗をコミットしてこの後youtubeとかを貼っていきたい。

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var imageContainers = imageList.getElementsByClassName("image-container");
 
     for (var i = 0; i < imageList.children.length; i++) {
+        console.log(imageContainers[i] + i);
         var images = imageContainers[i].getElementsByClassName("image");  // この行を修正
         var imageText = imageContainers[i].getElementsByClassName("image-text")[0];
         var gitLink = imageContainers[i].getElementsByClassName("git-links")[0];
@@ -13,12 +14,25 @@ document.addEventListener("DOMContentLoaded", function() {
             images[0].style.marginRight = "auto";  // 修正
             images[0].style.marginLeft = "10%";
             imageContainers[i].style.background = 'linear-gradient(to left,#FFF0B8,#FFF0B8,#FFD782)';
+
             imageText.style.left = "30%";
             gitLink.style.right = "16%";
-        } else {
+        }
+        else  if (i != 3) //赤霧の森だけ
+        {
             images[0].style.marginLeft = "auto";  // 修正
             images[0].style.marginRight = "10%";
             imageContainers[i].style.background = 'linear-gradient(to right,#CEF5FF,#E7C7FF)';
+            
+            imageText.style.right = "32%";
+            gitLink.style.left = "21%";
+        }
+        else
+        {
+            images[0].style.marginLeft = "auto";  // 修正
+            images[0].style.marginRight = "10%";
+            imageContainers[i].style.background = 'linear-gradient(to left, #8b0000, #320000, #320000, #320000, #000000, #000000)';
+            
             imageText.style.right = "32%";
             gitLink.style.left = "21%";
         }
